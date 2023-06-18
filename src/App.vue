f165cd13039981e4a801f55c2e9eaa8a5fe79b8e
Pratiksha <95347099+lastoyster@users.noreply.github.com>
2023-06-18 13:24:14 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg">
+    <Calculator/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator';
+export default {
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style >
+  .bg {
+    background-color: C77DFF;
+    width: 100vw;
+    height: 100vh;
+    padding: 2rem;
+  }
+  html, body {
+      font-family: 'Sora', sans-serif;
+  }
+</style>
